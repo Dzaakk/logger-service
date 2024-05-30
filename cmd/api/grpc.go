e0357305472eb18a085daed9d93ef4b8d9c84800
Dzkzzz <dzdzaakk@gmail.com>
2024-05-30 23:06:01 +0700

    fix typo

--- cmd/api/grpc.go
+++ cmd/api/grpc.go
@@ -37,7 +37,7 @@ func (l *LogServer) WriteLog(ctx context.Context, req *logs.LogRequest) (*logs.L
 }
 
 func (app *Config) gRPCListen() {
-	lis, err := net.Listen("tpc", fmt.Sprintf(":%s", gRpcPort))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", gRpcPort))
 	if err != nil {
 		log.Fatalf("Failed to listen for gRPC: %v", err)
 	}
